a142252ba50cfa525eb922cc2b16dca7d6ed749a
GB609 <39741460+GB609@users.noreply.github.com>
2026-08-21 23:49:09 +0200

    remove commented out debug code

--- src/build/run-tests.sh
+++ src/build/run-tests.sh
@@ -10,7 +10,7 @@ ROOT_DIR="$(realpath "$ROOT_DIR"/../..)"
 SRC_DIR="$ROOT_DIR"/src/js
 TESTSRC_DIR="$ROOT_DIR"/test/js
 
-shopt -s globstar dotglob
+shopt -s globstar dotglob nullglob
 
 TESTS=("${TESTSRC_DIR}"/**/*.test.{mj,j}s)
 
@@ -19,9 +19,6 @@ export ROOT_DIR SRC_DIR TESTSRC_DIR
 export NODE_PATH="$TESTSRC_DIR:$SRC_DIR"
 export REQUIRE_PATH="${ROOT_DIR}/src"
 
-#node
-
-#exit 0
 node --import "${SRC_DIR}/node-classy-test.mjs" \
   --trace-exit --trace-uncaught --trace-deprecation \
   --test-isolation=process \
